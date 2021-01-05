60dcee73c1b038309153236799551b801debab79
Yue Wu <yuewu57@gmail.com>
2021-01-05 23:02:10 +0000

    Update data_preprocessing_general.py

--- src/features/data_preprocessing_general.py
+++ src/features/data_preprocessing_general.py
@@ -9,24 +9,24 @@ from src.features.preprocessing_functions.data_cleaning import *
 
 if __name__ == '__main__':
 
-#     path='mat_files/'
-#     print("Loading and saving data from path:",path)
-#     loadParticipants_general(path)
-#     print("Making classes for data and saving in",DATA_interim)
-#     make_classes_general()
+    path='mat_files/'
+    print("Loading and saving data from path:",path)
+    loadParticipants_general(path)
+    print("Making classes for data and saving in",DATA_interim)
+    make_classes_general()
     
-#     participants=load_pickle(DATA_interim+'participants_class_general.pkl')
+    participants=load_pickle(DATA_interim+'participants_class_general.pkl')
     
-#     rule1,rule2,rule3='average','union','first'
+    rule1,rule2,rule3='average','union','first'
     
-#     print("Cleaning same day data using rule ",rule1," and aligning paired data using rule ",rule2)
-#     print("Saving first data for analysis after aligning data and excluding daily reported data, i.e., ids: ", exclude_ids)
-#     participants_aligned=aligning_data_general(cleaning_same_day_data(participants,rule=rule1),rule=rule2)
+    print("Cleaning same day data using rule ",rule1," and aligning paired data using rule ",rule2)
+    print("Saving first data for analysis after aligning data and excluding daily reported data, i.e., ids: ", exclude_ids)
+    participants_aligned=aligning_data_general(cleaning_same_day_data(participants,rule=rule1),rule=rule2)
     
-#     print("Cleaning same calendar week data using rule ",rule3," and saving as the second data for analysis")
+    print("Cleaning same calendar week data using rule ",rule3," and saving as the second data for analysis")
 
-#     cleaning_sameweek_data_general(participants_aligned,rule=rule3,save=True)
+    cleaning_sameweek_data_general(participants_aligned,rule=rule3,save=True)
 
-    subdirs=["participants_class_aligned_general.pkl","participants_class_weekly_general.pkl"]
+    subdirs=["participants_class_aligned_general.pkl", "participants_class_weekly_general.pkl"]
     for subdir in subdirs:
         clearning_redundant_missing_data(subdir,save=True)
